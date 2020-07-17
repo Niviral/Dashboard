90269356a5ae356035fb2b77becd3cf08136c7a9
m.bienias <m.bienias@tme.pl>
2020-07-17 08:59:59 +0200

    refresh button

--- search_time.py
+++ search_time.py
@@ -3,20 +3,15 @@ import dash
 import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
-class BuildDF:
-    def __init__(self):
-        self.df = pd.read_csv('hana_search.csv')
-        self.df3 = pd.read_csv('hana_search.csv').groupby(['DATE','QUERY_RAW_PHRASE']).agg(
-                min_logtime=('LOG_TIME',min),
-                max_logtime=('LOG_TIME',max),
-                mean_logtime=('LOG_TIME',"mean")
-                ).reset_index()
+df = pd.read_csv('hana_search.csv')
+df3 = pd.read_csv('hana_search.csv').groupby(['DATE','QUERY_RAW_PHRASE']).agg(
+    min_logtime=('LOG_TIME',min),
+    max_logtime=('LOG_TIME',max),
+    mean_logtime=('LOG_TIME',"mean")).reset_index()
 
-def get_df():
-    return BuildDF()
-t=get_df()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -28,40 +23,34 @@ colors = {
     'font':'#878b90'
 }
 
-phrase = t.df3['QUERY_RAW_PHRASE'].unique()
+phrase = df3['QUERY_RAW_PHRASE'].unique()
 
 app.layout = html.Div(children=[
-    html.H1(children=''),
     html.Div([
-        dcc.Dropdown(
-            id='dd_menu',
-            options=[
-            {'label': unique_phrase, 'value': unique_phrase} for unique_phrase in phrase],
-            value = 'kabel',
-            style=dict(
-                width='50%',
-                verticalAlign='left'
+            dcc.Dropdown(
+                id='dd_menu',
+                options=[
+                {'label': unique_phrase, 'value': unique_phrase} for unique_phrase in phrase],
+                value = 'kabel',
+                style=dict(
+                    width='50%',
+                    verticalAlign='left'
             )),
-        html.Button(
-            'Refresh', 
-            id='DF_refresh_button',
-            n_clicks=0,
-            style=dict(
-                width='200px',
-                verticalAlign='right'
-            )),
+            html.Button('Refresh',id='refresh_button')
     ],id='dd_div'),
     html.Div(id='search-time-graph-div'),
     html.Div(id='search-time-date-slider'),
     html.Div(id='search-time-dt-div'),
+    html.Div(id='tricky-div', style={'display': 'none'}),
 ])
 
 @app.callback(
     Output('search-time-graph-div','children'),
-    [Input('dd_menu', 'value')])
+    [Input('dd_menu', 'value'),
+    Input('tricky-div','children')])
 
-def update_graph(dd_menu_value):
-    ddf = t.df3[t.df3['QUERY_RAW_PHRASE']==dd_menu_value]
+def update_graph(dd_menu_value, value):
+    ddf = df3[df3['QUERY_RAW_PHRASE']==dd_menu_value]
     yax = [ddf['mean_logtime']*1.2,ddf['mean_logtime']*0.75]
 
     return [
@@ -84,19 +73,33 @@ def update_graph(dd_menu_value):
     ]
 @app.callback(
     Output('search-time-dt-div','children'),
-    [Input('dd_menu', 'value')])
+    [Input('dd_menu', 'value'),
+    Input('tricky-div','children')])
 
-def update_table(dd_menu_value):
+def update_table(dd_menu_value,value):
     return [
         dash_table.DataTable(
             id='hana_raw_data',
-            data = t.df[t.df['QUERY_RAW_PHRASE']==dd_menu_value].to_dict('records'),
-            columns=[{"name": i, "id": i} for i in t.df.columns],
+            data = df[df['QUERY_RAW_PHRASE']==dd_menu_value].to_dict('records'),
+            columns=[{"name": i, "id": i} for i in df.columns],
             page_size=10,
             filter_action='native',
             sort_action="native",
         )
     ]
+
+@app.callback(
+    Output('tricky-div','children'),
+    [Input('refresh_button','n_clicks')])
+def refresher(value):
+    global df
+    df = pd.read_csv('hana_search.csv')
+    global df3 
+    df3 = pd.read_csv('hana_search.csv').groupby(['DATE','QUERY_RAW_PHRASE']).agg(
+        min_logtime=('LOG_TIME',min),
+        max_logtime=('LOG_TIME',max),
+        mean_logtime=('LOG_TIME',"mean")).reset_index()
+    
 
 if __name__ == "__main__":
     app.run_server(host='0.0.0.0', port='8000',debug=True)
