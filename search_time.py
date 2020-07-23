820e44c92d37dc638ea0e9277ef968dd4f2790e9
m.bienias <m.bienias@tme.pl>
2020-07-23 14:22:08 +0200

    top menu fix and table changes

--- search_time.py
+++ search_time.py
@@ -8,8 +8,9 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
+date_format='%d-%m-%Y %H:%M'
 file_time_class = os.stat('hana_search.csv')
-file_time = time.asctime(time.localtime(file_time_class.st_mtime))
+file_time = time.strftime(date_format,time.localtime(file_time_class.st_mtime))
 df = pd.read_csv('hana_search.csv')
 df3 = pd.read_csv('hana_search.csv').groupby(['DATE','QUERY_RAW_PHRASE']).agg(
     min_logtime=('LOG_TIME',min),
@@ -22,25 +23,29 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__ ,external_stylesheets=external_stylesheets)
 server = app.server
 
-colors = {
-    'background': '#272b30',
-    'font':'#878b90'
-}
-
 phrase = df3['QUERY_RAW_PHRASE'].unique()
 
 app.layout = html.Div(children=[
     html.Div([
+        html.Div([
             dcc.Dropdown(
                 id='dd_menu',
                 options=[
                 {'label': unique_phrase, 'value': unique_phrase} for unique_phrase in phrase],
                 value = 'kabel',
-                style=dict(
-                    width='50%',
-                    verticalAlign='left'
-            )),
+            )
+        ],
+        style=dict(
+            width='68%',
+            display = 'table-cell',
+        )),
+        html.Div([
             html.Button('Refresh',id='refresh_button'),
+        ],
+        style=dict(
+            width='30%',
+            display = 'table-cell',
+        )),
     ],id='dd_div'),
     html.Div(id='search-time-graph-div'),
     html.Div(id='search-time-date-slider'),
@@ -55,7 +60,6 @@ app.layout = html.Div(children=[
 
 def update_graph(dd_menu_value, value):
     ddf = df3[df3['QUERY_RAW_PHRASE']==dd_menu_value]
-    yax = [ddf['mean_logtime']*1.2,ddf['mean_logtime']*0.75]
 
     return [
         dcc.Graph(
@@ -68,12 +72,9 @@ def update_graph(dd_menu_value, value):
                 ],
                 'layout': {
                     'title': f'Czas wyszukiwania {dd_menu_value}, {file_time}',
-                    'yaxis':{
-                        'range': [yax]
-                    }
                 }
             }
-        )
+            )
     ]
 @app.callback(
     Output('search-time-dt-div','children'),
@@ -86,9 +87,16 @@ def update_table(dd_menu_value,value):
             id='hana_raw_data',
             data = df[df['QUERY_RAW_PHRASE']==dd_menu_value].to_dict('records'),
             columns=[{"name": i, "id": i} for i in df.columns],
-            page_size=10,
+            fixed_rows={'headers':True},
+            style_table={"height": "400px","overflowY":"auto"},
+            page_size=50,
             filter_action='native',
-            sort_action="native",
+            sort_action='native',
+            style_data={
+                'whiteSpace': 'normal',
+                'height': '13px',
+                'lineHeight': '15px'
+            }
         )
     ]
 
