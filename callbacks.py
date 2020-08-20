3cd1080bb058cd884136990d18d9501656bbb7b2
m.bienias <m.bienias@tme.pl>
2020-08-20 08:02:08 +0200

    .

--- callbacks.py
+++ callbacks.py
@@ -8,16 +8,17 @@ import plotly.graph_objs as go
 from dash.dependencies import Input, Output
 from app import app
 
+FILE = os.environ['DASH_FILE_NAME']
 def refresher():
     date_format='%d-%m-%Y %H:%M'
-    file_time_class = os.stat(os.environ['DASH_FILE_NAME'])
+    file_time_class = os.stat(FILE)
     file_time = time.strftime(date_format,time.localtime(file_time_class.st_mtime))
-    df = pd.read_csv(os.environ['DASH_FILE_NAME'])
-    df3 = pd.read_csv(os.environ['DASH_FILE_NAME']).groupby(['DATE','QUERY_RAW_PHRASE']).agg(
+    df = pd.read_csv(FILE)
+    df_grouped = pd.read_csv(FILE).groupby(['DATE','QUERY_RAW_PHRASE']).agg(
         min_logtime=('LOG_TIME',min),
         max_logtime=('LOG_TIME',max),
         mean_logtime=('LOG_TIME',"mean")).reset_index()
-    return date_format, file_time, df, df3
+    return date_format, file_time, df, df_grouped
 
 @app.callback([
     Output('phrase_dd_menu','options'),
@@ -25,8 +26,8 @@ def refresher():
     [Input('type_dd_menu','value')])
 
 def return_phrase_dd(type_value):
-    date_format, file_time, df, df3 = refresher()
-    phrases = df3['QUERY_RAW_PHRASE'].unique()
+    date_format, file_time, df, df_grouped = refresher()
+    phrases = df_grouped['QUERY_RAW_PHRASE'].unique()
     options=[{'label':uniq_phrase, 'value':uniq_phrase} for uniq_phrase in phrases]
     value=phrases[0]
     return options, value
@@ -41,9 +42,9 @@ def return_phrase_dd(type_value):
     Input('radio_graph_picker','value')])
 
 def update_graph(phrase_value,type_value,n,end_date,start_date,radio_graph_value):
-    date_format, file_time, df, df3 = refresher()
+    date_format, file_time, df, df_grouped = refresher()
     if type_value == 'daily': 
-        ddf = df3[df3['QUERY_RAW_PHRASE'] == phrase_value]
+        ddf = df_grouped[df_grouped['QUERY_RAW_PHRASE'] == phrase_value]
         ddf = ddf.set_index('DATE')
         ddf.index = pd.to_datetime(ddf.index)
         ddf = ddf.asfreq('D').assign(QUERY_RAW_PHRASE=lambda x: x['QUERY_RAW_PHRASE'].ffill())
@@ -65,7 +66,7 @@ def update_graph(phrase_value,type_value,n,end_date,start_date,radio_graph_value
             )
         ]
     elif type_value == '15-min':
-        date_format, file_time, df, df3 = refresher()
+        date_format, file_time, df, df_grouped = refresher()
         ddf = df[df['QUERY_RAW_PHRASE'] == phrase_value]
         ddf['DATE_TIME'] = ddf['DATE'] + ' ' + ddf['TIME']
         ddf = ddf.drop(['DATE','TIME'], axis=1)
@@ -113,4 +114,3 @@ def update_table(dd_menu_value,n):
             }
         )
     ]
-    
